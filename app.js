47203f97a39fafeb69095bd5f13188f66004c166
samet <smtttlck@hotmail.com>
2023-08-26 16:30:24 +0300

    odev 8

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
+app.use(express.static('public'));
+
+app.set("view engine", "ejs");
+
 app.get('/', (req, res) => {
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
-    res.send(blog);
+    res.render('index');
+});
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+app.get('/add_post', (req, res) => {
+    res.render('add_post');
+});
+app.get('/post', (req, res) => {
+    res.render('post');
 });
 
 const port = 3000;
